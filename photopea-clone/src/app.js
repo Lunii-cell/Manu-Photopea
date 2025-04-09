bebeb6c81077c010e742d20c156c223e7b3c3179
Lunii-cell <lunifish0@gmail.com>
2025-04-09 23:39:29 +0000

    Iniciacion

--- photopea-clone/src/app.js
+++
@@ -1,9 +0,0 @@
-import { initializeCanvas } from './components/canvas.js';
-import { initializeSidebar } from './components/sidebar.js';
-import { initializeToolbar } from './components/toolbar.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  initializeToolbar();
-  initializeSidebar();
-  initializeCanvas();
-});
